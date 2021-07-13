867e6edd69434eef1d6758dad030e93942f963c0
Tom <twilliams@spartaglobal.com>
2021-07-13 11:35:45 +0100

    Vagrantfile and provision updated

--- environment/provision.sh
+++ environment/provision.sh
@@ -1,8 +1,8 @@
-!#/bin/bash
+#!/bin/bash
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install nginx 
-systemctl status nginx
+sudo apt-get install nginx -y
+# systemctl status nginx
 
 # This is written inside the VM
